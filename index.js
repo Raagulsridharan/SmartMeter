99f7248d59787abab877f2e9860b459929308700
Raagulsridharan <raagulsridharan01@gmail.com>
2023-04-23 16:32:22 +0530

    safe commit: Persisted

--- index.js
+++ index.js
@@ -1,50 +1,47 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const mqtt = require('mqtt');
+const Reading = require('./reading');
+const express = require('express');
+const mongoose = require('mongoose');
 
-const uri = "mongodb+srv://saravanakumarrc:sarapower@cluster0.omi9lez.mongodb.net/SmartMeter?retryWrites=true&w=majority";
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+//connecting to mqtt broker
+const client = mqtt.connect('mqtt://broker.hivemq.com')
+client.subscribe('KNOW_16042023_VOL');
+client.subscribe('KNOW_16042023_CUR');
+client.subscribe('KNOW_16042023_POW');
+
+// connect to mongodb
+mongoose.connect('mongodb+srv://saravanakumarrc:sarapower@cluster0.omi9lez.mongodb.net/SmartHomeDB?retryWrites=true&w=majority');
+mongoose.Promise = global.Promise;
+
+// set up our express app
+const app = express();
+app.use(express.static('public'));
+app.use(express.json());
+// initialize routes
+app.use('/api', require('./api'));
+
+// error handling middleware
+app.use(function (err, req, res, next) {
+	//console.log(err);
+	res.status(422).send({ error: err.message });
 });
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
-    const mqtt_client = mqtt.connect('mqtt://broker.hivemq.com')
-    mqtt_client.subscribe('KNOW_16042023_VOL')
-    mqtt_client.subscribe('KNOW_16042023_CUR')
-    mqtt_client.subscribe('KNOW_16042023_POW')
+// listen for requests
+app.listen(process.env.port || 4000, function () {
+	console.log('Ready to Go!');
+});
 
-    mqtt_client.on('message', (topic, message, packet) => {
-      console.log(`Received message on topic ${topic}: ${message.toString()}  : ${message.byteLength}`)
+client.on('message', (topic, message, packet) => {
+	console.log(`Received message on topic ${topic}: ${message.toString()}  : ${message.byteLength}`);
+	const reading = {
+		usage: Number(message.toString()),
+		usedAt: new Date(),
+		deviceId: "DEVICE1",
+		accountId: "ACCOUNT1",
+		unitType: topic
+	};
 
-      MongoClient.connect(uri, function (err, client) {
-        if (err) throw err;
-
-        const db = client.db('SmartMeter');
-        const collection = db.collection('readings');
-
-        const document = { topic, reading: message.toString(), datetime: new Date() };
-        collection.insertOne(document, function (err, result) {
-          if (err) throw err;
-
-          console.log('Document inserted:', result.insertedId);
-          client.close();
-        });
-      });
-    })
-
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+	Reading.create(reading).then(function (reading) {
+		console.log(`Persisted!`);
+	});
+});
---
+++ reading.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+// create reading schema & model
+const ReadingSchema = new Schema({
+    accountId: {
+        type: String,
+    },
+    deviceId: {
+        type: String,
+    },
+    usage: {
+        type: Number,
+        default: 0
+    },
+    usedAt: {
+        type: Date,
+    },
+    unitType:{
+        type: String,
+    }
+});
+const Reading = mongoose.model('reading', ReadingSchema);
+module.exports = Reading;
