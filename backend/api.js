147d9688e62c580de3ad7c574257111c67be90e2
saravanakumarrc <saravanakumarrc@gmail.com>
2023-04-26 21:51:13 +0530

    added get endpoints

--- backend/api.js
+++ backend/api.js
@@ -6,6 +6,33 @@ router.get('/readings', function (req, res, next) {
         res.send(readings);
     }).catch(next);
 });
+router.get('/readings/:deviceId', function (req, res, next) {
+    var query = { deviceId: req.params.deviceId };
+    var { unit, amount } = req.query;
+    if(unit && amount){
+        query = {
+            ...query,
+            $expr:{
+                $gt:
+                   [
+                      "$usedAt",
+                       {
+                          $dateSubtract:
+                             {
+                                startDate: "$$NOW",
+                                unit: unit,
+                                amount: Number(amount)
+                             }
+                       }
+                   ]
+             }   
+        }
+    }
+        
+    Reading.find(query).then(function (readings) {
+        res.send(readings);
+    }).catch(next);
+});
 router.post('/readings', function (req, res, next) {
     Reading.create(req.body).then(function (reading) {
         res.send(reading);
