22304041d6e3e53183a40fd44262c1dbc971070c
saravanakumarrc <saravanakumarrc@gmail.com>
2023-04-27 21:18:52 +0530

    added line chart

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgChartsModule } from 'ng2-charts';
+import { UnitsChartComponent } from './units-chart/units-chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UnitsChartComponent
   ],
   imports: [
     BrowserModule,
@@ -17,7 +21,9 @@ import { ServiceWorkerModule } from '@angular/service-worker';
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000'
-    })
+    }),
+    BrowserAnimationsModule,
+    NgChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
