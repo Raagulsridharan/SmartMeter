f954fab82d43bb65549c0425b30a5e81eac6707a
saravanakumarrc <saravanakumarrc@gmail.com>
2023-04-29 18:43:36 +0530

    working line chart

--- frontend/src/app/line-chart/line-chart.component.ts
+++ frontend/src/app/line-chart/line-chart.component.ts
@@ -4,6 +4,8 @@ import { Subject, takeUntil } from 'rxjs';
 import { AppService } from '../app.service';
 import { ChartOptions, Color } from 'chart.js';
 import { Chart, registerables } from 'chart.js';
+import 'chartjs-adapter-date-fns';
+import { de } from 'date-fns/locale';
 Chart.register(...registerables);
 
 @Component({
@@ -12,43 +14,34 @@ Chart.register(...registerables);
   styleUrls: ['./line-chart.component.css']
 })
 export class LineChartComponent implements OnInit {
-  constructor(private appService:AppService) {
+  constructor(private appService: AppService) {
   }
 
   ngOnInit() {
     var myChart = new Chart("canvas", {
-      type: 'bar',
+      type: 'line',
       data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
         datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
-          ],
-          borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
-          ],
-          borderWidth: 1
-        }]
-      },
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true
+          data: [{ usedAt: '2016-12-25 12:00', usage: 20 }, { usedAt: '2016-12-25 12:05', usage: 25 }, { usedAt: '2016-12-25 12:15', usage: 10 }],
+          parsing: {
+            xAxisKey: 'usedAt',
+            yAxisKey: 'usage'
           }
+        }]
+      }, options: {
+        scales: {
+          x: {
+            type: 'timeseries',
+            time: {
+              unit: 'minute'
+            },
+            adapters: {
+              date: {
+              }
+            }
+          },
         }
       }
     });
-  }  
+  }
 }
