c35c6a2163144f744e38cdcf3a7fca8058f4a81e
saravanakumarrc <saravanakumarrc@gmail.com>
2023-05-06 16:49:49 +0530

    hidden logout

--- frontend/src/app/toolbar/toolbar.component.ts
+++ frontend/src/app/toolbar/toolbar.component.ts
@@ -7,6 +7,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./toolbar.component.css']
 })
 export class ToolbarComponent {
+  deviceId = localStorage.getItem("deviceId");
   constructor(private router: Router){
     
   }
